75619868e7d19d16a781ea423a4fcdf52d3acb9d
Unknown <jagomez@up.edu.mx>
2017-06-10 13:36:58 -0500

    Actualizado

--- ShotgunTool.py
+++ ShotgunTool.py
@@ -20,27 +20,33 @@ def validateID(userInputID):
 	while validationNumber == False:
 		try:
 			userInputID = int(userInputID)
-			validationNumber = True
+			return userInputID
 		except:
 			userInputID = raw_input("ERROR! The ID must be a number\nType the ID of the %s:"%inputType)
 
 def validateIDShotgun (validatedID):
-	try:
-			shotgunValidation = False
-			print inputType
-			shotgunFile = sg.find_one(inputType, [["id", "is", validatedID]], ["id", "code"])
-		print shotgunFile
-	except Exception as e:
-		print e
-		#while shotgunValidation == False:
-		#if shotgunFile == None:
+	shotgunValidation = False
+	while shotgunValidation == False:
+		shotgunFile = sg.find_one(inputType, [["id", "is", validatedID]], ["id", "code", "sg_status_list"])
+		if shotgunFile == None:
+			print shotgunFile
+			print validatedID
+			newID = raw_input ("No %s founded in the project, try another ID:\n" %inputType)
+			try:
+				validatedID = validateID(newID)
+			except exception as e:
+				print e
+		else:
+			print "The %s found name is: %s \n" %(inputType, shotgunFile['code'])
+			return shotgunFile
 
 
 
 option = raw_input("What do you want to upload?\n->Asset\n->Shot\n").lower()
 inputType = validateType(option)
 ID = raw_input("Type the ID of the %s:"%inputType)
-inputID = validateID(ID)
+goodID = validateID(ID)
+print validateIDShotgun (goodID)
 
 print "Data correct"
 time.sleep (5)
